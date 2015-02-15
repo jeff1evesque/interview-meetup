03b0b73887b43259921772a6515070a01e7b65b7
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-15 13:20:24 -0500

    #27: toggle_favicon.js, define toggle logic

--- js/toggle_favicon.js
+++ js/toggle_favicon.js
@@ -3,3 +3,9 @@
  *                    when it is clicked. Specifically, the element toggles between
  *                    the two classnames, 'fa-star', and 'fa-star-o'.
  */
+
+$(document).ready(function() {
+  $('.fa').on('click', function(e) {
+    $(this).toggleClass('fa-star fa-star-o');
+  });
+});
