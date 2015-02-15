d496911a711a120a3055f8c2bfa8a7cfd5a346ff
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-15 14:08:32 -0500

    #35: ajax_data.js, fix typo, and send 'uid'

--- js/ajax_data.js
+++ js/ajax_data.js
@@ -1,6 +1,8 @@
 /**
  * ajax_data.js: this script utilizes ajax to relay the POST data corresponding
  *               to the clicked 'fa-star' element to a defined 'action' script.
+ *
+ * @uid, corresponds the user who is currently logged-in to the application.
  */
 
 $(document).ready(function() {
@@ -8,10 +10,13 @@ $(document).ready(function() {
   $('.figureset-button fa').on('click', function(event) {
     event.preventDefault();
 
+  // create data array:
+    var data_send = {class: $(this).attr('class'), uid: 0};
+
     $.ajax({
       type: 'POST',
       url: 'python/my_fave.py',
-      data: $(this).attr('class'),
+      data: data_send.serialize(),
       beforeSend: function() {
 
     // add ajax spinner
