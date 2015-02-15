994960662fb94cc0b5ba6eb7d62c315846970fec
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-15 13:14:26 -0500

    #27: toggle_favicon.js, add file with docblock

---
+++ js/toggle_favicon.js
@@ -0,0 +1,5 @@
+/**
+ * toggle_favicon.js: this script toggles the classname for the 'fa-star' element
+ *                    when it is clicked. Specifically, the element toggles between
+ *                    the two classnames, 'fa-star', and 'fa-star-o'.
+ */
