bcb7ffd7b5db3b856f3f59ab015717bf9bfee37a
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-15 20:53:42 -0500

    #50: app.py, define file

--- app.py
+++ app.py
@@ -2,3 +2,27 @@
 # This file loads corresponding logic, and html template file(s), which
 # allows the presentation of (asynchronous) content.
 from flask import Flask, render_template, request
+
+# Initialize: create flask instance
+app = Flask(__name__)
+
+# Define Route: assign corresponding template, or logic to given path
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/my_fave/', methods=['POST', 'GET'])
+def adjust_fave():
+  if request.method == 'POST':
+    # get POST data
+
+    # save fave
+
+    # delete fave
+    pass
+
+# Execute: run application directly, instead of import
+if __name__ == '__main__':
+  app.run(
+  debug=True
+)
