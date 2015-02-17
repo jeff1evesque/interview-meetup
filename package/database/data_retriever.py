9da4d5b126ff89ecbb4276151ccf6b3844a6c3d3
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-17 15:38:04 -0500

    #91: data_retriever.py, define class with constructor

--- package/database/data_retriever.py
+++ package/database/data_retriever.py
@@ -4,3 +4,11 @@
 #  This file contains logic required to select Meetup faves from the
 #      database.
 from package.database.db_query import SQL
+
+## Class: Get_Fave, explicitly inherit 'new-style' class
+class Get_Fave(object):
+
+  ## constructor
+  def __init__(self):
+    self.list_error = []
+    self.connector  = SQL()
