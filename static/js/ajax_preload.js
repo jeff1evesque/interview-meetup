8e78a05808a9b4d381f10b339e20b085c5ce08a2
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-17 11:50:19 -0500

    #84: ajax_preload.js, add file with docblock

---
+++ static/js/ajax_preload.js
@@ -0,0 +1,8 @@
+/**
+ * ajax_preload.js: this script utilizes ajax to determine if current search
+ *                  Meetup events are stored in the database as favorites.
+ *
+ *                  If contained within the database, the 'fave_button'
+ *                  element is given the css classname 'fa-star', otherwise
+ *                  the element will be given the css classname 'fa-star-o'.
+ */
