285b9f9fb75c0c742bdfa124dffad9721911c428
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-17 00:30:12 -0500

    #41: data_adjust_fave.py, add import statements

--- package/database/data_adjust_fave.py
+++ package/database/data_adjust_fave.py
@@ -3,3 +3,5 @@
 ## @db_query.py
 #  This file contains logic required to add, or remove Meetup faves from the
 #      database.
+from datetime import datetime
+from package.database.db_query import SQL  
