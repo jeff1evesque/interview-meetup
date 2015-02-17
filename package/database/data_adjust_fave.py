af603b1d2a52f2c5bbe279b1b8d85ae5e283ac97
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-17 00:32:41 -0500

    #41: data_adjust_fave.py, add class, constructor

--- package/database/data_adjust_fave.py
+++ package/database/data_adjust_fave.py
@@ -4,4 +4,11 @@
 #  This file contains logic required to add, or remove Meetup faves from the
 #      database.
 from datetime import datetime
-from package.database.db_query import SQL  
+from package.database.db_query import SQL
+
+## Class: Adjust_Fave, explicitly inherit 'new-style' class
+class Adjust_Fave(object):
+
+  ## constructor
+  def __init__(self):
+  self.list_error = []  
