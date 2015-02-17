ea2f2ef328d5d72e8b2aa4ea37996c4fa9593011
Jeff Levesque <jeff1evesque@yahoo.com>
2015-02-17 15:52:20 -0500

    #91: data_retriever.py, define 'fave_intersection'

--- package/database/data_retriever.py
+++ package/database/data_retriever.py
@@ -17,3 +17,25 @@ class Get_Fave(object):
   #                     intersection with stored favorite events with
   #                     respect to current user (uid).
   def fave_intersection( list_events, uid ):
+
+    # local variables
+    list_intersection = []
+
+    # create connection to sql
+    self.connector.sql_connect('db_my_faves')
+
+    for gid in list_events:
+      # insert 'fave' into 'tbl_fave_gid'
+      sql_statement = 'SELECT gid FROM tbl_fave_gid WHERE gid=%s AND uid_created=%s'
+      args = (gid, uid)
+      response = self.connector.sql_command( sql_statement, 'select', args )
+
+      if len(response['result']) > 0:
+        list_intersection.append( response['result'] )
+
+      # retrieve any error(s), end connection to sql
+      if self.connector.return_error(): self.list_error.append( self.connector.return_er$
+      self.connector.sql_disconnect()
+
+    # return list of intersections
+    return list_intersection
